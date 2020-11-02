e69fbedad79467d6637cb939dfab6af5a97fbd5b
Dante Iozzo <danteiozzo.gmail.com>
2020-11-02 12:06:05 -0500

    Add rotate_decomposition_basis to ModesTimeSeries
    f

--- scri/modes_time_series.py
+++ scri/modes_time_series.py
@@ -2,6 +2,8 @@ import copy
 import numpy as np
 from scipy.interpolate import CubicSpline
 import spherical_functions
+import quaternion
+from . import jit, Coprecessing, Coorbital, Corotating, Inertial
 
 
 class ModesTimeSeries(spherical_functions.Modes):
@@ -137,3 +139,111 @@ class ModesTimeSeries(spherical_functions.Modes):
     def ethbar_GHP(self):
         """Lower spin-weight with GHP convention"""
         return self.ethbar / np.sqrt(2)
+
+    from .mode_calculations_MTS import LLMatrix, LdtVector, angular_velocity
+
+    def rotate_physical_system(self, R_phys):
+        """Rotate a Waveform in place
+
+        This just rotates the decomposition basis by the inverse of the input
+        rotor(s).  See `rotate_decomposition_basis`.
+
+        For more information on the analytical details, see
+        http://moble.github.io/spherical_functions/SWSHs.html#rotating-swshs
+
+        """
+        rotated_data = rotate_decomposition_basis(self, ~R_phys)
+        return self  # Probably no return, but just in case...
+
+    def rotate_decomposition_basis(self, R_basis):
+        """Rotate a Waveform in place
+
+        This function takes a Waveform object `self` and either a quaternion
+        or array of quaternions `R_basis`.  It applies that rotation to
+        the decomposition basis of the modes in the Waveform.  The change
+        in basis is also recorded in the Waveform's `frame` data.
+
+        For more information on the analytical details, see
+        http://moble.github.io/spherical_functions/SWSHs.html#rotating-swshs
+
+        """
+        # This will be used in the jitted functions below to store the
+        # Wigner D matrix at each time step
+        D = np.empty((spherical_functions.WignerD._total_size_D_matrices(self.ell_min, self.ell_max),), dtype=complex)
+
+        rotated_data = self.copy()
+
+        if isinstance(R_basis, (list, np.ndarray)) and len(R_basis) == 1:
+            R_basis = R_basis[0]
+
+        if isinstance(R_basis, (list, np.ndarray)):
+            if isinstance(R_basis, np.ndarray) and R_basis.ndim != 1:
+                raise ValueError("Input dimension mismatch.  R_basis.shape={1}".format(R_basis.shape))
+            if self.shape[0] != len(R_basis):
+                raise ValueError(
+                    "Input dimension mismatch.  (self.shape[0]={}) != (len(R_basis)={})".format(
+                        self.shape[0], len(R_basis)
+                    )
+                )
+            _rotate_decomposition_basis_by_series(
+                rotated_data, quaternion.as_spinor_array(R_basis), self.ell_min, self.ell_max, D
+            )
+
+        # We can't just use an `else` here because we need to process the
+        # case where the input was an iterable of length 1, which we've
+        # now changed to just a single quaternion.
+        if isinstance(R_basis, np.quaternion):
+            spherical_functions._Wigner_D_matrices(R_basis.a, R_basis.b, self.ell_min, self.ell_max, D)
+            tmp = np.empty((2 * self.ell_max + 1,), dtype=complex)
+            _rotate_decomposition_basis_by_constant(rotated_data, self.ell_min, self.ell_max, D, tmp)
+
+        return rotated_data
+
+
+@jit("void(c16[:,:], i8, i8, c16[:], c16[:])")
+def _rotate_decomposition_basis_by_constant(data, ell_min, ell_max, D, tmp):
+    """Rotate data by the same rotor at each point in time
+
+    `D` is the Wigner D matrix for all the ell values.
+
+    `tmp` is just a workspace used as temporary storage to hold the
+    results for each item of data during the sum.
+
+    """
+    for i_t in range(data.shape[0]):
+        for ell in range(ell_min, ell_max + 1):
+            i_data = ell ** 2 - ell_min ** 2
+            i_D = spherical_functions._linear_matrix_offset(ell, ell_min)
+
+            for i_m in range(2 * ell + 1):
+                tmp[i_m] = 0j
+            for i_mp in range(2 * ell + 1):
+                for i_m in range(2 * ell + 1):
+                    tmp[i_m] += data[i_t, i_data + i_mp] * D[i_D + (2 * ell + 1) * i_mp + i_m]
+            for i_m in range(2 * ell + 1):
+                data[i_t, i_data + i_m] = tmp[i_m]
+
+
+@jit("void(c16[:,:], c16[:,:], i8, i8, c16[:])")
+def _rotate_decomposition_basis_by_series(data, R_basis, ell_min, ell_max, D):
+    """Rotate data by a different rotor at each point in time
+
+    `D` is just a workspace, which holds the Wigner D matrices.
+    During the summation, it is also used as temporary storage to hold
+    the results for each item of data, where the first row in the
+    matrix is overwritten with the new sums.
+
+    """
+    for i_t in range(data.shape[0]):
+        spherical_functions._Wigner_D_matrices(R_basis[i_t, 0], R_basis[i_t, 1], ell_min, ell_max, D)
+        for ell in range(ell_min, ell_max + 1):
+            i_data = ell ** 2 - ell_min ** 2
+            i_D = spherical_functions._linear_matrix_offset(ell, ell_min)
+
+            for i_m in range(2 * ell + 1):
+                new_data_mp = 0j
+                for i_mp in range(2 * ell + 1):
+                    new_data_mp += data[i_t, i_data + i_mp] * D[i_D + i_m + (2 * ell + 1) * i_mp]
+                D[i_D + i_m] = new_data_mp
+            for i_m in range(2 * ell + 1):
+                data[i_t, i_data + i_m] = D[i_D + i_m]
